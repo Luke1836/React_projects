89cb3d8ccc58d2b7a386fb2229f3e8447fbeb4b7
Luke1836 <codingluke2023@gmail.com>
2024-04-20 22:04:15 +0530

    New components added

---
+++ Project-1/vite-project/src/Components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function navbar() {
+    return (
+        <nav className='navbar'>
+            <dl>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Services</li>
+            </dl>
+        </nav>
+    );
+}
+
+export default navbar;
