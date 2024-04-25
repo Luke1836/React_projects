4d394968c1dc1862b51d995e19e045e76d2fdde5
Luke1836 <codingluke2023@gmail.com>
2024-04-25 11:38:50 +0530

    Styles added - 5

--- Project-1/vite-project/src/Components/background.jsx
+++ Project-1/vite-project/src/Components/background.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
 
 function Background() {
     return (
         <div className="icon">
-            <FontAwesomeIcon icon="fa-brands fa-react" size="2xl" style={{color: "#003e8f",}} />
+            <FontAwesomeIcon icon={faReact} size="10x" style={{ color: "#003e8f" }} />
         </div>
     )
 }
--- Project-1/vite-project/src/main.jsx
+++ Project-1/vite-project/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 import './index.css'
+import './Components/background.css'
 import Navbar from './Components/navbar.jsx'
 import Background from './Components/background.jsx'
 
