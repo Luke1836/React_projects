dcbc5ef58f856b86e314c11308c89bb8cfd6e47b
Luke1836 <codingluke2023@gmail.com>
2024-04-25 11:30:13 +0530

    Styles added - 4

---
+++ Project-1/vite-project/src/Components/background.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Background() {
+    return (
+        <div className="icon">
+            <FontAwesomeIcon icon="fa-brands fa-react" size="2xl" style={{color: "#003e8f",}} />
+        </div>
+    )
+}
+
+export default Background;
--- Project-1/vite-project/src/main.jsx
+++ Project-1/vite-project/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './App.css'
 import './index.css'
 import Navbar from './Components/navbar.jsx'
+import Background from './Components/background.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Background />
     <Navbar />
     <App />
   </React.StrictMode>,
