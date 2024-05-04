4884357ba357c230d4474425b1b76034ccb62630
Luke1836 <codingluke2023@gmail.com>
2024-05-04 10:34:02 +0530

    Intro to props

--- Project-2/vite-project/src/App.jsx
+++ Project-2/vite-project/src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import './App.css';
 
-function App() {
+function App(props) {
     return (
         <div className="card">
             <div className="image_sec">
-                <img src={} loading="lazy" alt={} />
+                <img src={props.name} loading="lazy" alt={props.name} />
             </div>
             <div className="description">
-                <h1>{}</h1>
-                <p></p>
+                <h1>{props.name}</h1>
+                <p>
+                    <h2>Occupation: {props.occupation}</h2>
+                    <h2>Salary: {props.salary}</h2>
+                </p>
             </div>
         </div>
     );
 }
+
+export default App;
--- Project-2/vite-project/src/main.jsx
+++ Project-2/vite-project/src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App name="Luke Ericsson" occupation="Financial Analyst" salary="200000" />
+    <App name="George Kurian Thomas" occupation="Chief Finance Officer" salary="800000" />
+    <App name="Alexis Morgan" occupation="HR" salary="30000" />
+    <App name="Mike Evergreen" occupation="Portfolio Manager" salary="300000" />
   </React.StrictMode>,
 )
