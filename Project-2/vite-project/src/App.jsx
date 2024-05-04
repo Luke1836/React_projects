e1ac7d47a41e1f1a098bdc331cfadec7539e2020
Luke1836 <codingluke2023@gmail.com>
2024-05-04 09:52:12 +0530

    Handling raw data

--- Project-2/vite-project/src/App.jsx
+++ Project-2/vite-project/src/App.jsx
@@ -1,2 +1,16 @@
 import React from "react";
 import './App.css';
+
+function App() {
+    return (
+        <div className="card">
+            <div className="image_sec">
+                <img src={} loading="lazy" alt={} />
+            </div>
+            <div className="description">
+                <h1>{}</h1>
+                <p></p>
+            </div>
+        </div>
+    );
+}
