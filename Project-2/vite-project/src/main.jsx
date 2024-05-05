6ac0dd523924efcc78e6884363cf8246a122dd66
Luke1836 <codingluke2023@gmail.com>
2024-05-05 18:24:15 +0530

    Styles changed

--- Project-2/vite-project/src/main.jsx
+++ Project-2/vite-project/src/main.jsx
@@ -9,5 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App img="../Public/Images/Nyiragongo-Volcano-Congo.avif" name="George Kurian Thomas" occupation="Chief Finance Officer" salary="800000" />
     <App img="../Public/Images/St.Helens.avif" name="Alexis Morgan" occupation="HR" salary="30000" />
     <App img="../Public/Images/waterfall-3.avif" name="Mike Evergreen" occupation="Portfolio Manager" salary="300000" />
+    <App img="../Public/Images/St.Helens.avif" name="Alexis Morgan" occupation="HR" salary="30000" />
+    <App img="../Public/Images/waterfall-3.avif" name="Mike Evergreen" occupation="Portfolio Manager" salary="300000" />
   </React.StrictMode>,
 )
