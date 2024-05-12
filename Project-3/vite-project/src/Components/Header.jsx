9e1806de7bcbfcc26f51fbbd5e7a21a399c51a1e
Luke1836 <codingluke2023@gmail.com>
2024-05-12 21:34:07 +0530

    Header styles added - 3

--- Project-3/vite-project/src/Components/Header.jsx
+++ Project-3/vite-project/src/Components/Header.jsx
@@ -4,9 +4,10 @@ function Header() {
     return (
         <header className="header">
             <div className="logo">
+                <img src="../Public/Troll Face.png" alt="Troll Face" loading="lazy" className="icon"/>
                 MEMEGENERATOR
             </div>
-            <div className="logo">
+            <div className="about">
                 Project-3 (React)
             </div>
         </header>
