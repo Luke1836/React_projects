0d59bba65a0b621d74dfc4110544c8ab0e5d01fd
Luke1836 <codingluke2023@gmail.com>
2024-05-12 20:24:07 +0530

    Header compnent added -1

---
+++ Project-3/vite-project/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="logo">
+                MEMEGENERATOR
+            </div>
+            <div className="logo">
+                Project-3 (React)
+            </div>
+        </header>
+    );
+}
+export default Header;
