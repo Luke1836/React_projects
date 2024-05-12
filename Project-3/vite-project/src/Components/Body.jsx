a03da8e8e22656e9f315160fe3485c2c2fc78299
Luke1836 <codingluke2023@gmail.com>
2024-05-12 21:37:53 +0530

    Body compoent added

---
+++ Project-3/vite-project/src/Components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Body() {
+    return (
+        <div className="hero">
+            <div className="input-container">
+                <input type="text" />
+                <input type="text" />
+            </div>
+        </div>
+    );
+}
