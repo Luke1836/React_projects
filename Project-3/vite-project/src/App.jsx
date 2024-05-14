0c7d71dbd3af9290765a9b2a512a472b8cc21dfa
Luke1836 <codingluke2023@gmail.com>
2024-05-14 23:09:52 +0530

    API linked

--- Project-3/vite-project/src/App.jsx
+++ Project-3/vite-project/src/App.jsx
@@ -2,9 +2,4 @@ import React from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="container-memes">
-        
-    </div>
-  );
 }
--- Project-3/vite-project/src/Components/Body.jsx
+++ Project-3/vite-project/src/Components/Body.jsx
@@ -8,25 +8,30 @@ function Body() {
         const memes = memeData.data.memes;
         const MemeNumber = Math.floor((Math.random() * memes.length));
         setmemeImage(memes[MemeNumber].url);    //Since we don't care about the previous state of the state variable
-        const randomMeme = memes[MemeNumber];
-        console.log(randomMeme);
     }
+
     return (
         <div className="hero">
-            <div className="input-container">
-                <div className="form">
-                    <label htmlFor="ttext">Top Text</label>
-                    <input type="text" placeholder="Enter" id="ttext" name="ttext" />
+            <div className="initial-section">
+                <div className="input-container">
+                    <div className="form">
+                        <label htmlFor="ttext">Top Text</label>
+                        <input type="text" placeholder="Enter" id="ttext" name="ttext" />
+                    </div>
+                    
+                    <div className="form">
+                        <label htmlFor="btext">Bottom Text</label>
+                        <input type="text" placeholder="Enter" id="btext" name="btext" />
+                    </div>
                 </div>
-                
-                <div className="form">
-                    <label htmlFor="btext">Bottom Text</label>
-                    <input type="text" placeholder="Enter" id="btext" name="btext" />
+
+                <div className="buttons">
+                    <button type="button" className="generator" onClick={memeGenerator}><span>Get a new meme image</span><img src="../Public/Geneartor.png" /></button>
                 </div>
             </div>
-
-            <div className="buttons">
-                <button type="button" className="generator" onClick={memeGenerator}><span>Get a new meme image</span><img src="../Public/Geneartor.png" /></button>
+            
+            <div className="memes-container">
+                <img src={memeImage} loading="lazy"/>
             </div>
         </div>
     );
