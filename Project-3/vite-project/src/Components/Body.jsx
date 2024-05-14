aed5915a3787554113d6ae7157dbd1f91d11c8b4
Luke1836 <codingluke2023@gmail.com>
2024-05-14 21:19:55 +0530

    State varible and function added

--- Project-3/vite-project/src/Components/Body.jsx
+++ Project-3/vite-project/src/Components/Body.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import memeData from '../Database/info.js';
 
-function memeGenerator() {
-    const memes = memeData.data.memes;
-    const MemeNumber = Math.floor((Math.random() * memes.length));
-    const randomMeme = memes[MemeNumber];
-    console.log(randomMeme);
-}
-
 function Body() {
+
+    const [memeImage, setmemeImage] = useState("");
+    function memeGenerator() {
+        const memes = memeData.data.memes;
+        const MemeNumber = Math.floor((Math.random() * memes.length));
+        setmemeImage(memes[MemeNumber].url);    //Since we don't care about the previous state of the state variable
+        const randomMeme = memes[MemeNumber];
+        console.log(randomMeme);
+    }
     return (
         <div className="hero">
             <div className="input-container">
