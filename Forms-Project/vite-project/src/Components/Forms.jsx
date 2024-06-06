c9ea0cbebac1333227e5b6609007a0f07a248c21
Luke1836 <codingluke2023@gmail.com>
2024-06-06 13:25:24 +0530

    Forms structure completed

--- Forms-Project/vite-project/src/Components/Forms.jsx
+++ Forms-Project/vite-project/src/Components/Forms.jsx
@@ -4,9 +4,42 @@ function Forms()
 {
 
     return (
-        <form>
-            
-        </form>
+        <div className="formContainer">
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="email"
+                    placeholder="Email Address"
+                    className="form-input"
+                    name="email"
+                    onChange={handleChange}
+                    value={FormData.email}
+                />
+
+                <input 
+                    type="password"
+                    placeholder="Password"
+                    className="form-input"
+                    name="password"
+                    onChange={handleChange}
+                    value={FormData.password}
+                />
+
+                <div className="form--marketing">
+                    <input
+                        id="okayToEmail"
+                        type="checkbox"
+                        name="joinedNewsletter"
+                        onChange={handleChange}
+                        checked={formData.joinedNewsletter}
+                    />
+                    <label htmlFor="okayToEmail">I want to join the newsletter</label>
+                </div>
+                
+                <button className="form--submit">
+                    Sign up
+                </button>
+            </form>
+        </div>
     )
 }
 
