c0ac252578be0ebc0989afd6e80cd61fdf5bc007
Luke1836 <codingluke2023@gmail.com>
2024-06-19 19:52:40 +0530

    App component changed

--- Project-4/vite-project/src/App.jsx
+++ Project-4/vite-project/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from './Components/Header.jsx'
+import Main from './Components/Main.jsx'
 
 function App() {
-
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
 }
 
 export default App;
