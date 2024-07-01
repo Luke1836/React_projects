3a7152f219c5feaab1ba37dc55f0496bb9614152
Luke1836 <codingluke2023@gmail.com>
2024-07-01 14:34:21 +0530

    Dark mode button addition 3

--- Project-1/vite-project/src/App.jsx
+++ Project-1/vite-project/src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import Navbar from './Components/navbar.jsx'
+import React, { useState, useRef } from 'react';
+import Navbar from './Components/navbar.jsx';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const sliderRef = useRef(null);
+
+  function handleChange() {
+    setDarkMode(prevState => !prevState);
+    sliderRef.current.classList.toggle('active');
+  }
+
   return (
     <>
-      <Navbar />
+      <Navbar darkMode={darkMode} toggleDarkMode={handleChange} sliderRef={sliderRef} />
       <div className='background'>
         <h1>Fun Facts About React</h1>
         <ul className='list-points'>
           <li>Was first released in 2013.</li>
           <li>Was originally created by Jordan Walke.</li>
           <li>Has well over 100k stars on GitHub.</li>
-          <li>Maintained by facebook.</li>
+          <li>Maintained by Facebook.</li>
           <li>Powers thousands of enterprises apps, including mobile apps.</li>
         </ul>
       </div>
--- Project-1/vite-project/src/Components/navbar.jsx
+++ Project-1/vite-project/src/Components/navbar.jsx
@@ -3,21 +3,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact } from '@fortawesome/free-brands-svg-icons';
 import Button from './Button';
 
-function Navbar() {
+function Navbar(props) {
     return (
-        <div className='pos'>
-            <nav className='navbar'>
-                <FontAwesomeIcon icon={faReact} size="4x" style={{ color: "#003e8f" }} />
-                <dl>
-                    <dd>Home</dd>
-                    <dd>About</dd>
-                    <dd>Contact</dd>
-                    <dd>Services</dd>
-                </dl>
-                <Button />
-            </nav>
-        </div>
+      <div className='pos'>
+        <nav className={props.darkMode ? 'lightMode' : 'navbar'}>
+          <FontAwesomeIcon icon={faReact} size="4x" style={{ color: "#003e8f" }} />
+          <dl>
+            <dd>Home</dd>
+            <dd>About</dd>
+            <dd>Contact</dd>
+            <dd>Services</dd>
+          </dl>
+          <Button toggleDarkMode={props.toggleDarkMode} sliderRef={props.sliderRef}/>
+        </nav>
+      </div>
     );
-}
-
-export default Navbar;
+  }
+  
+  export default Navbar;
