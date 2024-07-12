2762ce4ff7892f55ef9e873864b0315d432f7408
Luke1836 <codingluke2023@gmail.com>
2024-07-12 19:55:03 +0530

    New Porject changes added

--- Project-4/vite-project/src/App.jsx
+++ Project-4/vite-project/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import Header from './Components/Header.jsx'
-import Main from './Components/Main.jsx'
+import Header from './Components/Editor.jsx'
+import Main from './Components/Sidebar.jsx'
+import ReactMde from "react-mde";
+import 'react-mde/lib/styles/css/react-mde-all.css';
 
 function App() {
     return (
