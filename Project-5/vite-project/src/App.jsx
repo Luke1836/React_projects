bb65ccde26293656f9bb286fa484ae2119682ed9
Luke1836 <codingluke2023@gmail.com>
2024-07-20 20:56:43 +0530

    Project - 5 Styles changed -2

--- Project-5/vite-project/src/App.jsx
+++ Project-5/vite-project/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Main from './Components/Main'
 import './App.css'
 
 function App() {
-  
+    return (
+        <>
+            <Main />
+        </>
+    )   
 }
 
 export default App
