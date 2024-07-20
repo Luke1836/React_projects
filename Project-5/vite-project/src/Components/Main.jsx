a5a2b3c531450b07748288d4fa3b8642d1f3ebfe
Luke1836 <codingluke2023@gmail.com>
2024-07-20 21:12:01 +0530

    Project - 5 Styles changed - 5

--- Project-5/vite-project/src/Components/Main.jsx
+++ Project-5/vite-project/src/Components/Main.jsx
@@ -6,11 +6,11 @@ function Main() {
         <main className="main">
             <div className="dice-container">
                 <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
                 <Die value="1" />
                 <Die value="1" />
                 <Die value="1" />
