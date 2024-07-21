473fc88e872a189443b5d615d08fcf8084ae2fa7
Luke1836 <codingluke2023@gmail.com>
2024-07-21 14:22:15 +0530

    Project - 5 Underline added

--- Project-5/vite-project/src/Components/Description.jsx
+++ Project-5/vite-project/src/Components/Description.jsx
@@ -4,6 +4,7 @@ function Description() {
     return (
         <div className="hero">
             <h1 className="decript-header">Tenzies</h1>
+            <div className="underline"></div>
             <p className="p-description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         </div>
     )
