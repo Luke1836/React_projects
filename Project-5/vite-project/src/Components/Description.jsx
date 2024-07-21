f0fb6c155fd296080913a4dde7d243955877885a
Luke1836 <codingluke2023@gmail.com>
2024-07-21 14:12:21 +0530

    Project - 5 New Description component added

---
+++ Project-5/vite-project/src/Components/Description.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Description() {
+    return (
+        <div className="hero">
+            <h1 className="decript-header">Tenzies</h1>
+            <p className="p-description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        </div>
+    )
+}
+
+export default Description;
