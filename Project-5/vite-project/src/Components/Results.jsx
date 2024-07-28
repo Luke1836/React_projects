1848d8f0d1333aca2a883bc59719f312552a714d
Luke1836 <codingluke2023@gmail.com>
2024-07-28 20:35:35 +0530

    LocalStorage -4

--- Project-5/vite-project/src/Components/Results.jsx
+++ Project-5/vite-project/src/Components/Results.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 
 function Results(props) {
-    
+
     return (
         <div className="results-section">
             <h2>No: of throws: {props.moves}</h2>
-            <h2>Best Record: {props.minMoves === 0 ? "null" : props.minMoves}</h2>
+            <h2>Best Record: {props.minMoves === 100 ? "0" : props.minMoves}</h2>
         </div>
     );
 }
