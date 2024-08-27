f03a20b48c87b5e3d1b790aebcbd14a5c5e76b2b
Luke1836 <codingluke2023@gmail.com>
2024-08-28 00:39:54 +0530

    Header component added and styled

--- Project-6/vite-project/src/App.jsx
+++ Project-6/vite-project/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Header from "./Components/Header";
 
 export default function App()
 {
     return (
-        <>
-        </>
+        <div>
+            <Header />
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
     );
 }
---
+++ Project-6/vite-project/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() 
+{
+    return (
+        <header>
+            <img src="react-core-concepts.png" alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                Fundamental React concepts you will need for almost any app you are
+                going to build!
+            </p>
+        </header>
+    );
+}
+
+export default Header;
