14d5a018a25cb31136ad1c4501fc332e3f296920
Luke1836 <codingluke2023@gmail.com>
2024-09-10 19:37:56 +0530

    Components added

--- Project-7/vite-project/src/App.jsx
+++ Project-7/vite-project/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Board from "./Components/Board";
 
 function App()
 {
     return (
         <>
-        
+            <Header />
+            <Board />
+            <Footer />
         </>
     );
 }
