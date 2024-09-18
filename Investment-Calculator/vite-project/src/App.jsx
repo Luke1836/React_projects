3b4659edbb85bead8e5068788f7831e00ffeabac
Luke1836 <codingluke2023@gmail.com>
2024-09-18 23:19:33 +0530

    Investment calculator userInput  added

--- Investment-Calculator/vite-project/src/App.jsx
+++ Investment-Calculator/vite-project/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import UserInputs from "./components/UserInputs";
 
 function App()
 {
     return (
         <>
-            
+            <UserInputs />
         </>
     )
 }
