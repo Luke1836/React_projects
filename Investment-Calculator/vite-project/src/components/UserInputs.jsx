4df9e81e78ef564ccb538cf33a20f30a9629e391
Luke1836 <codingluke2023@gmail.com>
2024-09-19 23:40:22 +0530

    Investment calculator component state updated

--- Investment-Calculator/vite-project/src/components/UserInputs.jsx
+++ Investment-Calculator/vite-project/src/components/UserInputs.jsx
@@ -24,22 +24,38 @@ export default function UserInputs()
             <div className="input-group">
                 <p>
                     <label>Initial Investment</label>
-                    <input type="number" required onChange={(event) => handleChange('initialInvestment', event.target.value)} />
+                    <input type="number" 
+                        required 
+                        onChange={(event) => handleChange('initialInvestment', event.target.value)}
+                        value={userInputs.initialInvestment} 
+                    />
                 </p>
                 <p>
                     <label>Annual Investment</label>
-                    <input type="number" required onChange={(event) => handleChange('annualInvestment', event.target.value)} />
+                    <input type="number" 
+                        required 
+                        onChange={(event) => handleChange('annualInvestment', event.target.value)}
+                        value={userInputs.annualInvestment}    
+                    />
                 </p>
             </div>
             
             <div className="input-group">
                 <p>
                     <label>Expected Return</label>
-                    <input type="number" required onChange={(event) => handleChange('expectedReturn', event.target.value)} />
+                    <input type="number" 
+                        required 
+                        onChange={(event) => handleChange('expectedReturn', event.target.value)} 
+                        value={userInputs.expectedReturn}    
+                    />
                 </p>
                 <p>
                     <label>Duration</label>
-                    <input type="number" required onChange={(event) => handleChange('duration', event.target.value)} />
+                    <input type="number" 
+                        required 
+                        onChange={(event) => handleChange('duration', event.target.value)} 
+                        value={userInputs.duration}    
+                    />
                 </p>
             </div>
         </div>
