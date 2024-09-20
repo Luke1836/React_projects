42a1d904bcdcb86620eb45733609c9f565889a94
Luke1836 <codingluke2023@gmail.com>
2024-09-20 20:38:47 +0530

    Investment calculator Results component re-updated

--- Investment-Calculator/vite-project/src/components/Results.jsx
+++ Investment-Calculator/vite-project/src/components/Results.jsx
@@ -21,13 +21,14 @@ function Results({ userInputs })
                     resultsData.map(yearData => {
                         const initialInvestment = userInputs.initialInvestment;
                         const totalInterest = yearData.valueEndOfYear - (yearData.annualInvestment * yearData.year) - initialInvestment;
+                        const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
                         return (
                             <tr key={yearData.year}>
                                 <td>{yearData.year}</td>
                                 <td>{formatter.format(yearData.valueEndOfYear)}</td>
                                 <td>{formatter.format(yearData.interest)}</td>
                                 <td>{formatter.format(totalInterest)}</td>
-                                <td>{formatter.format(yearData.annualInvestment)}</td>
+                                <td>{formatter.format(totalAmountInvested)}</td>
                             </tr>
                         )
                     })
