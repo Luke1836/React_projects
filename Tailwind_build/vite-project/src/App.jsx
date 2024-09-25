aae7ace0d7a9bb2d32d33bf2499c3401b7a8e950
Luke1836 <codingluke2023@gmail.com>
2024-09-25 21:22:25 +0530

    Styles changed 3

--- Tailwind_build/vite-project/src/App.jsx
+++ Tailwind_build/vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Components/Header";
+import Home from "./Components/Home";
 
 function App()
 {
   return (
     <>
       <Header />
+      <Home />
     </>
   )
 }
---
+++ Tailwind_build/vite-project/src/Components/Home.jsx
@@ -0,0 +1,10 @@
+function Home()
+{
+    return (
+        <section id="home" className="h-svh ">
+            
+        </section>
+    )
+}
+
+export default Home;
