80fe806200cf26b5daa73a93139cf1a6993f1a8c
Luke1836 <codingluke2023@gmail.com>
2024-09-25 20:02:01 +0530

    Components revised

---
+++ Tailwind_build/vite-project/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+function Header()
+{
+    return (
+        <div className="">
+            <img src="../assets/Tailwind-logo.png" alt="Tailwindcss logo" />
+            <h1>The Styling King</h1>
+            <dl>
+                <dd>Home</dd>
+                <dd>About</dd>
+                <dd>Components</dd>
+                <dd>Projects</dd>
+            </dl>
+        </div>
+    )
+}
+
+export default Header;
--- Tailwind_build/vite-project/tailwind.config.js
+++ Tailwind_build/vite-project/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        title: ['"Nunito Sans"', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
