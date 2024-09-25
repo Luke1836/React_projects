472a87b46007270d04b886a7ee5825a30aeeadf7
Luke1836 <codingluke2023@gmail.com>
2024-09-25 21:06:22 +0530

    Styles changed 2

--- Tailwind_build/vite-project/src/Components/Header.jsx
+++ Tailwind_build/vite-project/src/Components/Header.jsx
@@ -3,14 +3,14 @@ function Header()
     return (
         <header className="flex flex-row flex-nowrap justify-between items-center text-slate-300 lg:w-12/13 m-auto p-2 border-b-2 border-b-slate-800">
             <div className="flex-none flex flex-row items-center gap-4">
-                <img className="size-12 m-3 ml-12 inline" src="../src/assets/tailwindcss.png" alt="Tailwindcss logo" />
-                <h1 className="text-3xl md:text-xl inline">The Styling Kings</h1>
+                <img className="size-12 md:m-1 lg:m-3 lg:ml-12 inline cursor-pointer" src="../src/assets/tailwindcss.png" alt="Tailwindcss logo" />
+                <h1 className="lg:text-3xl md:text-xl inline cursor-default">The Styling Kings</h1>
             </div>
-            <dl className="flex-none text-xl font-title">
-                <dd className="inline me-12 cursor-pointer">Home</dd>
-                <dd className="inline me-12 cursor-pointer">About</dd>
-                <dd className="inline me-12 cursor-pointer">Components</dd>
-                <dd className="inline me-12 cursor-pointer">Projects</dd>
+            <dl className="hidden md:block text-base lg:text-xl font-title">
+                <dd className="inline me-6 lg:me-12 cursor-pointer hover:text-cyan-400">Home</dd>
+                <dd className="inline me-6 lg:me-12 cursor-pointer hover:text-cyan-400">About</dd>
+                <dd className="inline me-6 lg:me-12 cursor-pointer hover:text-cyan-400">Components</dd>
+                <dd className="inline me-6 lg:me-12 cursor-pointer hover:text-cyan-400">Projects</dd>
             </dl>
         </header>
     )
