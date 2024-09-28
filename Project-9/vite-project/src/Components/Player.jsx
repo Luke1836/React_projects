40e4b87adb9264056d0e3e59f62fadd5e139de6c
Luke1836 <codingluke2023@gmail.com>
2024-09-29 00:31:48 +0530

    Styles changed

--- Project-9/vite-project/src/Components/Player.jsx
+++ Project-9/vite-project/src/Components/Player.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react' 
+
 export default function Player() 
 {
+    const [playerName, setPlayerName] = useState('')
     return (
       <section id="player">
-        <h2>Welcome unknown entity</h2>
+        <h2>Welcome { playerName ? playerName : 'Unknown Entity'}</h2>
         <p>
-          <input type="text" />
+          <input type="text" value={playerName} placeholder='Enter your name'/>
           <button>Set Name</button>
         </p>
       </section>
