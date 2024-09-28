f72ac8463ee1e7aabb833efc70e0d2cf120a6434
Luke1836 <codingluke2023@gmail.com>
2024-09-29 00:46:32 +0530

    Component state added

--- Project-9/vite-project/src/Components/Player.jsx
+++ Project-9/vite-project/src/Components/Player.jsx
@@ -3,12 +3,25 @@ import { useState } from 'react'
 export default function Player() 
 {
     const [playerName, setPlayerName] = useState('')
+    const [state, setState] = useState(false);
+
+    function handleChange(event)
+    {
+        setState(false)
+        setPlayerName(event.target.value)
+    }
+
+    function handleClick()
+    {
+        setState(true)
+    }
+
     return (
       <section id="player">
-        <h2>Welcome { playerName ? playerName : 'Unknown Entity'}</h2>
+        <h2>Welcome { state ? `${playerName}!` : 'Player1!'}</h2>
         <p>
-          <input type="text" value={playerName} placeholder='Enter your name'/>
-          <button>Set Name</button>
+          <input type="text" value={playerName} placeholder='Enter your name' onChange={handleChange}/>
+          <button onClick={ handleClick }>Set Name</button>
         </p>
       </section>
     );
