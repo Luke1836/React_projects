08972f5ca944cfb93efc1539df782611d07d6acb
Luke1836 <codingluke2023@gmail.com>
2024-09-29 23:19:12 +0530

    Functionality increaed 2

--- Project-9/vite-project/src/Components/Player.jsx
+++ Project-9/vite-project/src/Components/Player.jsx
@@ -3,17 +3,17 @@ import { useState, useRef } from 'react'
 export default function Player() 
 {
     const playerNameChange = useRef();
-    const [playerName, setPlayerName] = useState('');
+    const [playerName, setPlayerName] = useState(null);
 
     function handleClick()
     {
-        setPlayerName(playerNameChange.current.value)
+        setPlayerName(playerNameChange.current.value);
         playerNameChange.current.value = ''
     }
 
     return (
       <section id="player">
-        <h2>Welcome { playerName ? `${playerName}!` : 'Player1!'}</h2>
+        <h2>Welcome { playerName ?? 'Player1'}!</h2>
         <p>
           <input ref={ playerNameChange } type="text" placeholder='Enter your name' />
           <button onClick={ handleClick }>Set Name</button>
