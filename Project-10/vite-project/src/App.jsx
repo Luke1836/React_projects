2cc7b1b0add843ec149d192da8b713de583bd0a7
Luke1836 <codingluke2023@gmail.com>
2024-10-04 20:38:53 +0530

    New component added

--- Project-10/vite-project/src/App.jsx
+++ Project-10/vite-project/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import ProjectsSidebar from "./Components/ProjectsSidebar";
 
 function App()
 {
     return (
-      <div className="relative w-svw h-svh">
-        <h1 className="text-emerald-500 text-center text-2xl absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          Hello! And Welcome
-        </h1>
-      </div>
+      <main className="h-screen my-8">
+        <ProjectsSidebar />
+      </main>
     )
 }
 
---
+++ Project-10/vite-project/src/Components/ProjectsSidebar.jsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+function ProjectsSidebar()
+{
+    return (
+        <aside className='w-1/3 px-8 py-16 bg-stone-800 text-slate-100 md:w-72 rounded-r-lg'>
+            <h2 className='mb-8 font-bold uppercase md:text-xl'>
+                Your projects
+            </h2>
+            <div>
+                <button className='px-4 py-2 text-xs md:text-base rounded-md  bg-stone-700 text-stone-400 hover:text-stone-300 hover:bg-stone-500 active:text-stone-200 active:p-3'>
+                    <FontAwesomeIcon icon={faPlus}/> Add Project
+                </button>
+            </div>
+        </aside>
+    )
+}
+
+export default ProjectsSidebar;
