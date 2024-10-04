dc4afbe09b4cd2a2387648544a056bd94fb175fb
Luke1836 <codingluke2023@gmail.com>
2024-10-04 19:53:34 +0530

    Initial files updated

--- Project-10/vite-project/src/App.jsx
+++ Project-10/vite-project/src/App.jsx
@@ -1,9 +1,14 @@
-function App() {
-  return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+import React from "react";
+
+function App()
+{
+    return (
+      <div className="relative w-svw h-svh">
+        <h1 className="text-emerald-500 text-center text-2xl absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          Hello! And Welcome
+        </h1>
+      </div>
+    )
 }
 
 export default App;
