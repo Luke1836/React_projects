89dc3daa1ddfb9cfbd86ea0ec2ae22aea25a4220
Luke1836 <codingluke2023@gmail.com>
2024-10-04 19:29:01 +0530

    Initial files updated

--- Project-10/vite-project/src/App.jsx
+++ Project-10/vite-project/src/App.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-
-function App()
-{
-    return (
-      <>
-      
-      </>
-    )
+function App() {
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </>
+  );
 }
 
 export default App;
