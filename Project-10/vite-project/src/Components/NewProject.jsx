bc6e784450d86af1c7862d75408a4caa21bac16a
Luke1836 <codingluke2023@gmail.com>
2024-10-05 13:25:10 +0530

    Stylings added 2

--- Project-10/vite-project/src/Components/NewProject.jsx
+++ Project-10/vite-project/src/Components/NewProject.jsx
@@ -4,15 +4,15 @@ import Input from "./Input";
 function NewProject()
 {
     return (
-        <div>
-            <menu>
+        <div className="w-[35rem] pt-16">
+            <menu className="flex items-center justify-end pr-[1rem] gap-2">
                 <li>
-                    <button>
+                    <button className= "text-stone-800 px-2 py-1 rounded-sm font-semibold hover:text-violet-900">
                         Cancel
                     </button>
                 </li>
                 <li>
-                    <button>
+                    <button className="bg-stone-800 text-slate-200 px-4 py-1 rounded-md font-semibold">
                         Save
                     </button>
                 </li>
