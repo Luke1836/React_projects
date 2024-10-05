433c15404e966b9f8693a20c235c2f639c2fc044
Luke1836 <codingluke2023@gmail.com>
2024-10-05 13:47:29 +0530

    Stylings changed 2

--- Project-10/vite-project/src/Components/Input.jsx
+++ Project-10/vite-project/src/Components/Input.jsx
@@ -2,8 +2,11 @@ export default function Input( { label, textarea, ...args} )
 {
     return (
         <p className="flex flex-col gap-1 my-4">
-            <label>{label}</label>
-            { textarea ? <textarea {...args}/> : <input {...args} /> }
+            <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+            { textarea ? 
+                    <textarea {...args} className="pl-2 bg-stone-300 rounded-md h-[8rem] border-2 focus:outline-none focus:border-stone-800"/> 
+                    : 
+                    <input {...args} className="pl-2 bg-stone-300 rounded-md h-[2rem] focus:outline-none border-2 focus:border-stone-800" /> }
         </p>
     )
 }
