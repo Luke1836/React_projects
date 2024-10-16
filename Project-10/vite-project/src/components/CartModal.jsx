aa799e2fbd6c66d8b1c31c43359cec23becd248e
Luke1836 <codingluke2023@gmail.com>
2024-10-16 20:02:30 +0530

    New project added

---
+++ Project-10/vite-project/src/components/CartModal.jsx
@@ -0,0 +1,29 @@
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import Cart from './Cart';
+
+const CartModal = forwardRef(function Modal({ cartItems, onUpdateCartItemQuantity, title, actions }, ref) 
+{
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog id="modal" ref={dialog}>
+      <h2>{title}</h2>
+      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      <form method="dialog" id="modal-actions">
+        {actions}
+      </form>
+    </dialog>,
+    document.getElementById('modal')
+  );
+});
+
+export default CartModal;
