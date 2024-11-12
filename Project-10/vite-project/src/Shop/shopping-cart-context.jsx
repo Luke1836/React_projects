1bd2af2a6b6e8c515dcf5c100b78ec9a739f2334
Luke1836 <codingluke2023@gmail.com>
2024-11-12 09:01:43 +0530

    Reduecer emthepod added 3

--- Project-10/vite-project/src/Shop/shopping-cart-context.jsx
+++ Project-10/vite-project/src/Shop/shopping-cart-context.jsx
@@ -11,48 +11,67 @@ export const CartContext = createContext({
 
 function ShoppingCartReducer(state, action)
 {
+  if(action.type === 'ADD_ITEM')
+  {
+    const updatedItems = [...prevShoppingCart.items];
+    
+    // Get the cart item selected
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === id
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+    
+    if (existingCartItem) 
+    {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } 
+    else 
+    {
+      const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+      updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+      });
+    }
+    return {
+      ...state, //Saving the previous state 
+      items: updatedItems,
+    };
+  }
+  else if(action.type === 'UPDATE_ITEM')
+  {
+
+  }
+  
   return state;
 }
 
 export default function CartContextProvider({ children })
 {
-    const [ shoppingCartState, shoppingCartDispatch ] = useReducer();
-    
+    //Reducer State and Dispatching function
+    const [ shoppingCartState, shoppingCartDispatch ] = useReducer(
+      ShoppingCartReducer, 
+      {
+        items: [],
+      }
+    );  // Giving the initial state as the second parameter
+
     const [shoppingCart, setShoppingCart] = useState({ items: [], });
     
       function handleAddItemToCart(id) 
       {
+        shoppingCartDispatch({
+          type: 'Add_ITEM',
+          payload: id
+        });
         setShoppingCart(( prevShoppingCart ) => {
-          const updatedItems = [...prevShoppingCart.items];
-    
-          // Get the cart item selected
-          const existingCartItemIndex = updatedItems.findIndex(
-            (cartItem) => cartItem.id === id
-          );
-          const existingCartItem = updatedItems[existingCartItemIndex];
-    
-          if (existingCartItem) 
-          {
-            const updatedItem = {
-              ...existingCartItem,
-              quantity: existingCartItem.quantity + 1,
-            };
-            updatedItems[existingCartItemIndex] = updatedItem;
-          } 
-          else 
-          {
-            const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-            updatedItems.push({
-              id: id,
-              name: product.title,
-              price: product.price,
-              quantity: 1,
-            });
-          }
-    
-          return {
-            items: updatedItems,
-          };
+          
         });
       }
     
