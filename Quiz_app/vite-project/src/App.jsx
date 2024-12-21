fa1f2377cccce63c9da264e444793942ad73b11b
Luke1836 <codingluke2023@gmail.com>
2024-12-21 20:20:16 +0530

    Quiz question sheet added

--- Quiz_app/vite-project/src/App.jsx
+++ Quiz_app/vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './Components/Header.jsx'
+import Quiz from './Components/Quiz.jsx'
 
 function App() {
   
   return (
     <>
       <Header />
+      <Quiz />
     </>
   )
 }
--- Quiz_app/vite-project/src/Components/Quiz.jsx
+++ Quiz_app/vite-project/src/Components/Quiz.jsx
@@ -1,6 +1,28 @@
 import { useState } from 'react';
+import QUESTIONS from '../questions.js'
 
 function Quiz()
 {
-    const [ activeQuestion, setActiveQuestion ]
+    const [ userAnswers, setUserAnswers] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer( selectedAnswer )
+    {
+        setUserAnswers(prevAnswers => ([ ...prevAnswers, selectedAnswer ]));
+    }
+    
+    return (
+        <div id='question'>
+            <h2>{ QUESTIONS[activeQuestionIndex].text }</h2>
+            <ul id='answers'>
+                { QUESTIONS[activeQuestionIndex].answers.map((answer) => 
+                    (<li key={ answer } className='answer'>
+                        <button onClick={() => handleSelectAnswer(answer) }>{ answer }</button>
+                    </li>)
+                )}
+            </ul>
+        </div>
+    );
 }
+
+export default Quiz;
