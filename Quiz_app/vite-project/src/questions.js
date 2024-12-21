01224237ebc2be800ac9d8e2a0c68fb0b4f34154
Luke1836 <codingluke2023@gmail.com>
2024-12-21 19:52:58 +0530

    Quiz local dataset added

---
+++ Quiz_app/vite-project/src/questions.js
@@ -0,0 +1,202 @@
+export default [
+    {
+      id: 'q1',
+      text: 'Which of the following definitions best describes React.js?',
+      answers: [
+        'A library to build user interfaces with help of declarative code.',
+        'A library for managing state in web applications.',
+        'A framework to build user interfaces with help of imperative code.',
+        'A library used for building mobile applications only.',
+      ],
+    },
+    {
+      id: 'q2',
+      text: 'What purpose do React hooks serve?',
+      answers: [
+        'Enabling the use of state and other React features in functional components.',
+        'Creating responsive layouts in React applications.',
+        'Handling errors within the application.',
+        'Part of the Redux library for managing global state.',
+      ],
+    },
+    {
+      id: 'q3',
+      text: 'Can you identify what JSX is?',
+      answers: [
+        'A JavaScript extension that adds HTML-like syntax to JavaScript.',
+        'A JavaScript library for building dynamic user interfaces.',
+        'A specific HTML version that was explicitly created for React.',
+        'A tool for making HTTP requests in a React application.',
+      ],
+    },
+    {
+      id: 'q4',
+      text: 'What is the most common way to create a component in React?',
+      answers: [
+        'By defining a JavaScript function that returns a renderable value.',
+        'By defining a custom HTML tag in JavaScript.',
+        'By creating a file with a .jsx extension.',
+        'By using the "new" keyword followed by the component name.',
+      ],
+    },
+    {
+      id: 'q5',
+      text: 'What does the term "React state" imply?',
+      answers: [
+        'An object in a component that holds values and may cause the component to render on change.',
+        'The lifecycle phase a React component is in.',
+        'The overall status of a React application, including all props and components.',
+        'A library for managing global state in React applications.',
+      ],
+    },
+    {
+      id: 'q6',
+      text: 'How do you typically render list content in React apps?',
+      answers: [
+        'By using the map() method to iterate over an array of data and returning JSX.',
+        'By using the for() loop to iterate over an array of data and returning JSX.',
+        'By using the forEach() method to iterate over an array of data and returning JSX.',
+        'By using the loop() method to iterate over an array of data and returning JSX.',
+      ],
+    },
+    {
+      id: 'q7',
+      text: 'Which approach can NOT be used to render content conditionally?',
+      answers: [
+        'Using a the #if template syntax.',
+        'Using a ternary operator.',
+        'Using the && operator.',
+        'Using an if-else statement.',
+      ],
+    },
+    {
+        id: 'q8',
+        text: 'What is the purpose of the HTML <meta> tag?',
+        answers: [
+          'To provide metadata about the HTML document, such as character set and viewport settings.',
+          'To insert a comment into the HTML document.',
+          'To create links between pages on the same website.',
+          'To define styles for the entire HTML document.',
+        ],
+      },
+      {
+        id: 'q9',
+        text: 'Which HTTP method is used to request data from a server?',
+        answers: [
+          'GET',
+          'POST',
+          'PUT',
+          'DELETE',
+        ],
+      },
+      {
+        id: 'q10',
+        text: 'Which CSS property is used to change the text color of an element?',
+        answers: [
+          'color',
+          'text-color',
+          'font-color',
+          'background-color',
+        ],
+      },
+      {
+        id: 'q11',
+        text: 'What is the purpose of the JavaScript "use strict" directive?',
+        answers: [
+          'To enforce stricter parsing and error handling in JavaScript code.',
+          'To define constants in JavaScript.',
+          'To optimize JavaScript code for performance.',
+          'To enable asynchronous operations in JavaScript.',
+        ],
+      },
+      {
+        id: 'q12',
+        text: 'Which of the following is a CSS layout model?',
+        answers: [
+          'Flexbox',
+          'Pixels',
+          'Box-sizing',
+          'Z-index',
+        ],
+      },
+      {
+        id: 'q13',
+        text: 'What is the main purpose of the "Promise" object in JavaScript?',
+        answers: [
+          'To handle asynchronous operations and their results.',
+          'To improve application performance by caching results.',
+          'To provide a secure mechanism for user authentication.',
+          'To create reusable components in JavaScript.',
+        ],
+      },
+      {
+        id: 'q14',
+        text: 'Which of the following is a valid way to include JavaScript in an HTML document?',
+        answers: [
+          '<script src="script.js"></script>',
+          '<js src="script.js"></js>',
+          '<code src="script.js"></code>',
+          '<java src="script.js"></java>',
+        ],
+      },
+      {
+        id: 'q15',
+        text: 'Which SQL statement is used to fetch data from a database?',
+        answers: [
+          'SELECT',
+          'FETCH',
+          'GET',
+          'PULL',
+        ],
+      },
+      {
+        id: 'q16',
+        text: 'What does the "alt" attribute in an HTML <img> tag specify?',
+        answers: [
+          'A text alternative for an image when it cannot be displayed.',
+          'The title of the image.',
+          'The file path of the image.',
+          'The dimensions of the image.',
+        ],
+      },
+      {
+        id: 'q17',
+        text: 'Which of the following is a front-end JavaScript framework?',
+        answers: [
+          'Angular',
+          'Django',
+          'Flask',
+          'Laravel',
+        ],
+      },
+      {
+        id: 'q18',
+        text: 'What is the primary function of the "box-shadow" property in CSS?',
+        answers: [
+          'To add shadows to elements.',
+          'To add margins to elements.',
+          'To change the border radius of elements.',
+          'To specify the width of an element.',
+        ],
+      },
+      {
+        id: 'q19',
+        text: 'Which of the following is NOT a valid CSS value unit?',
+        answers: [
+          'fps',
+          'px',
+          'em',
+          'vh',
+        ],
+      },
+      {
+        id: 'q20',
+        text: 'What is the purpose of the "Content-Type" header in HTTP requests?',
+        answers: [
+          'To specify the media type of the resource being sent or requested.',
+          'To set the HTTP method being used.',
+          'To define caching instructions for the request.',
+          'To include authorization credentials for the request.',
+        ],
+      },
+  ];
