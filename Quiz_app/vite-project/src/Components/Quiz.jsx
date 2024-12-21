5c76c05a538e8df67e70ad6809cd9d2aab03d960
Luke1836 <codingluke2023@gmail.com>
2024-12-21 20:45:53 +0530

    Quiz question sheet updated 5

--- Quiz_app/vite-project/src/Components/Quiz.jsx
+++ Quiz_app/vite-project/src/Components/Quiz.jsx
@@ -1,22 +1,41 @@
 import { useState } from 'react';
 import QUESTIONS from '../questions.js'
+import TrophyIcon from '../assets/quiz-complete.png'
 
 function Quiz()
 {
     const [ userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
+    const isQuizComplete = activeQuestionIndex ===  QUESTIONS.length;
 
     function handleSelectAnswer( selectedAnswer )
     {
         setUserAnswers(prevAnswers => ([ ...prevAnswers, selectedAnswer ]));
     }
+
+    if(isQuizComplete)
+    {
+        return (
+            <div id='summary'>
+                <img src={ TrophyIcon } alt='Trophy Icon' />
+                <h1>Quiz Complete</h1>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [ ...QUESTIONS[activeQuestionIndex].answers ];
+    for (let i = shuffledAnswers.length - 1; i > 0; i--) 
+    {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledAnswers[i], shuffledAnswers[j]] = [shuffledAnswers[j], shuffledAnswers[i]];
+    }  // Shuffles the answers
     
     return (
         <div id="quiz">
             <div id='question'>
-                <h2 id='text-shadow'>{ QUESTIONS[activeQuestionIndex].text }</h2>
+                <h2>{ QUESTIONS[activeQuestionIndex].text }</h2>
                 <ul id='answers'>
-                    { QUESTIONS[activeQuestionIndex].answers.map((answer) => 
+                    { shuffledAnswers.map((answer) => 
                         (<li key={ answer } className='answer'>
                             <button onClick={() => handleSelectAnswer(answer) }>{ answer }</button>
                         </li>)
