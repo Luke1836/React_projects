3741418862319e10d169d1cfb7c6d983b4bbf3d5
Luke1836 <codingluke2023@gmail.com>
2025-01-26 00:26:53 +0530

    Styles changed

--- Quiz_app/vite-project/src/Components/Quiz.jsx
+++ Quiz_app/vite-project/src/Components/Quiz.jsx
@@ -4,8 +4,11 @@ import Summary from './Summary.jsx';
 import Question from './Question.jsx';
 
 
-export default function Quiz() {
+export default function Quiz() 
+{
   const [userAnswers, setUserAnswers] = useState([]);
+  const [ startQuiz, setStartQuiz ] = useState(false);
+
 
   const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
@@ -24,20 +27,38 @@ export default function Quiz() {
     [handleSelectAnswer]
   );
 
-  if (quizIsComplete) {
+  function handleStartQuiz()
+  {
+    setStartQuiz(true);
+  }
+
+  // Results of the Quiz
+  if( quizIsComplete ) 
+  {
     return (
       <Summary userAnswers={ userAnswers }/>
     );
   }
 
+  // Begin the Quiz
+  if( startQuiz )
+  {
+    return (
+      <div id="quiz">
+        <Question
+          key={activeQuestionIndex}
+          index={activeQuestionIndex}
+          onSelectAnswer={handleSelectAnswer}
+          onSkipAnswer={handleSkipAnswer}
+        />
+      </div>
+    )
+  }
   return (
-    <div id="quiz">
-      <Question
-        key={activeQuestionIndex}
-        index={activeQuestionIndex}
-        onSelectAnswer={handleSelectAnswer}
-        onSkipAnswer={handleSkipAnswer}
-      />
+    <div id='quiz'>
+      <button className='start' onClick={ handleStartQuiz }>
+        Start Quiz
+      </button>
     </div>
   );
 }
