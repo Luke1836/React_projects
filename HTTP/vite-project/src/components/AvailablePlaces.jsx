91fc3bd917f17582827f202e39c401b5181e4afe
Luke1836 <codingluke2023@gmail.com>
2025-02-25 12:14:47 +0530

    Changes made 3

--- HTTP/vite-project/src/components/AvailablePlaces.jsx
+++ HTTP/vite-project/src/components/AvailablePlaces.jsx
@@ -3,22 +3,28 @@ import { useEffect, useState } from 'react'
 
 export default function AvailablePlaces({ onSelectPlace }) 
 {
+  const [ fetchingData, setFetchingData ] = useState();
   const [ availablePlaces, setAvailablePlaces ] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      })
+    async function fetchData()
+    {
+      setFetchingData(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setFetchingData(false);
+    }
+    
+    fetchData();
   }, []);
 
-  
+
   return (
     <Places
       title="Available Places"
+      isLoading={ fetchingData }
+      loadingText="Fetching Data...."
       places={ availablePlaces }
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
