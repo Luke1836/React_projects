f81a27f2786989f84d83950c02387ba256c81356
Luke1836 <codingluke2023@gmail.com>
2025-02-25 13:03:24 +0530

    Error handling added

--- HTTP/vite-project/src/components/AvailablePlaces.jsx
+++ HTTP/vite-project/src/components/AvailablePlaces.jsx
@@ -1,25 +1,42 @@
 import Places from './Places.jsx';
 import { useEffect, useState } from 'react'
+import Error from './Error.jsx';
+
 
 export default function AvailablePlaces({ onSelectPlace }) 
 {
   const [ fetchingData, setFetchingData ] = useState();
   const [ availablePlaces, setAvailablePlaces ] = useState([]);
+  const [ error, setError ] = useState();
 
   useEffect(() => {
     async function fetchData()
     {
-      setFetchingData(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        setFetchingData(true);
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+        setAvailablePlaces(resData.places);
+        if(!response.ok)
+          throw new Error('Failed to fetch the data!');
+      }
+      catch(error)
+      {
+        setError({
+          message: error.message || "Couldn't fetch the data! Please try again later!" 
+        })
+      }
+      
       setFetchingData(false);
     }
     
     fetchData();
   }, []);
 
-
+  if(error)
+    return (
+      <Error title="An Error has occured" message={ error.message } />
+    )
   return (
     <Places
       title="Available Places"
