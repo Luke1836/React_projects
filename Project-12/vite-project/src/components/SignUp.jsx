ef2a705939f7b94cf758336c6ab85d0f7b21f63f
Luke1836 <codingluke2023@gmail.com>
2025-02-26 21:07:18 +0530

    Updated the form 2

--- Project-12/vite-project/src/components/SignUp.jsx
+++ Project-12/vite-project/src/components/SignUp.jsx
@@ -2,8 +2,20 @@ import { useState } from 'react';
 
 function SignUp() 
 {
+  function handleSubmit(event)
+  {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const acquisitionData = fd.getAll('acquisition');
+    const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquisitionData;
+    console.log(data);
+    
+  }
+
+
   return (
-    <form>
+    <form onSubmit={ handleSubmit }>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
