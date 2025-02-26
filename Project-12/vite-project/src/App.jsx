317283c022c15349aec9221437e16bb55b25fad7
Luke1836 <codingluke2023@gmail.com>
2025-02-26 20:52:38 +0530

    Sign up added

--- Project-12/vite-project/src/App.jsx
+++ Project-12/vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header.jsx';
 import Login from './components/Login.jsx';
+import SignUp from './components/SignUp.jsx';
 
-function App() {
+function App() 
+{
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <SignUp />
       </main>
     </>
   );
